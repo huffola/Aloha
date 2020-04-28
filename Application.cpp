3d4f56937488660510ab94762a7429883e6d93da
Aaron Huff <huffola@gmail.com>
2020-04-28 15:10:07 -0400

    Update Application.cpp
    Turrets work for color change!!!!

--- Application.cpp
+++ Application.cpp
@@ -2,130 +2,6 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <Windows.h>
-#include <glut.h>
-
-
-
-bool towerProx(int placed)
-{
-     return false;
-}
-
-void turret(int num)
-{
-     GLubyte r = 102;
-     GLubyte g = 0;
-     GLubyte b = 204;
-
-     switch (num)
-     {
-     case 1:
-
-          //Position 2 Turret
-          glBegin(GL_QUADS); //will be set to begin if MouseControl activates
-          glColor3ub(r, g, b); // set color purple
-          glVertex3f(-0.25f, .875f, 0.1f);
-          glVertex3f(-0.375, .75f, 0.1f);
-          glVertex3f(-0.25f, .625f, 0.1f);
-          glVertex3f(-0.125f, 0.75f, 0.1f);
-          glEnd();
-          break;
-
-     case 2:
-          //Position 15 Turret
-          glBegin(GL_QUADS);
-          glColor3ub(r, g, b); // set color purple
-          glVertex3f(0.25f, -.625f, 0.1f);
-          glVertex3f(0.125, -.75f, 0.1f);
-          glVertex3f(0.25f, -0.875f, 0.1f);
-          glVertex3f(0.375f, -0.75f, 0.1f);
-          glEnd();
-          break;
-     }
-}
-
-bool damage()
-
-{
-     bool towerproximity = towerProx(1);
-     if (towerproximity)
-          return true;
-     else
-          return false;
-}
-
-void enemy(int num) //Written By Ryan
-{
-     GLubyte r = 255;
-     GLubyte g = 0;
-     GLubyte b = 0;
-     bool isdamaged = damage();
-     switch (num)
-     {
-     case 1: glBegin(GL_TRIANGLES); //Position 3
-          if (isdamaged)
-          {
-               g = 255;
-               b = 255;
-          }
-          glColor3ub(r, g, b);
-          glVertex2f(0.25f, 0.9f);
-          glVertex2f(0.1f, 0.6f);
-          glVertex2f(0.4f, 0.6f);
-          glEnd();
-          break;
-     case 2: glBegin(GL_TRIANGLES); //Position 6
-          if (isdamaged)
-          {
-               g = 255;
-               b = 255;
-          }
-          glColor3ub(r, g, b);
-          glVertex2f(0.25f, 0.4f);
-          glVertex2f(0.1f, 0.1f);
-          glVertex2f(0.4f, 0.1f);
-          glEnd();
-          break;
-     case 3: glBegin(GL_TRIANGLES); //Position 7
-          if (isdamaged)
-          {
-               g = 255;
-               b = 255;
-          }
-          glColor3ub(r, g, b);
-          glVertex2f(-0.25f, 0.4f);
-          glVertex2f(-0.4f, 0.1f);
-          glVertex2f(-0.1f, 0.1f);
-          glEnd();
-          break;
-     case 4: glBegin(GL_TRIANGLES); //Position 10
-          if (isdamaged)
-          {
-               g = 255;
-               b = 255;
-          }
-          glColor3ub(r, g, b);
-          glVertex2f(-0.25f, -0.1f);
-          glVertex2f(-0.4f, -0.4f);
-          glVertex2f(-0.1f, -0.4f);
-          glEnd();
-
-          break;
-     case 5: glBegin(GL_TRIANGLES); //Position 14
-          if (isdamaged)
-          {
-               g = 255;
-               b = 255;
-          }
-          glColor3ub(r, g, b);
-          glVertex2f(-0.25f, -0.6f);
-          glVertex2f(-0.4f, -0.9f);
-          glVertex2f(-0.1f, -0.9f);
-          glEnd();
-          break;
-
-     }
-}
 
 void map()
 {
@@ -292,6 +168,137 @@ void map()
 
 }
 
+void placeholders()
+{
+     
+}
+
+bool towerProx(int placed)
+{
+     return false;
+}
+
+bool damage()
+
+{
+     bool towerproximity = towerProx(1);
+     if (towerproximity)
+          return true;
+     else
+          return false;
+}
+
+
+
+void enemy(int num) //Written By Ryan
+{
+     GLubyte r = 255;
+     GLubyte g = 0;
+     GLubyte b = 0;
+     bool isdamaged = damage();
+     switch (num)
+     {
+     case 1: glBegin(GL_TRIANGLES); //Position 3
+          if (isdamaged)
+          {
+               g = 255;
+               b = 255;
+          }
+          glColor3ub(r, g, b);
+          glVertex2f(0.25f, 0.9f);
+          glVertex2f(0.1f, 0.6f);
+          glVertex2f(0.4f, 0.6f);
+          glEnd();
+          break;
+     case 2: glBegin(GL_TRIANGLES); //Position 6
+          if (isdamaged)
+          {
+               g = 255;
+               b = 255;
+          }
+          glColor3ub(r, g, b);
+          glVertex2f(0.25f, 0.4f);
+          glVertex2f(0.1f, 0.1f);
+          glVertex2f(0.4f, 0.1f);
+          glEnd();
+          break;
+     case 3: glBegin(GL_TRIANGLES); //Position 7
+          if (isdamaged)
+          {
+               g = 255;
+               b = 255;
+          }
+          glColor3ub(r, g, b);
+          glVertex2f(-0.25f, 0.4f);
+          glVertex2f(-0.4f, 0.1f);
+          glVertex2f(-0.1f, 0.1f);
+          glEnd();
+          break;
+     case 4: glBegin(GL_TRIANGLES); //Position 10
+          if (isdamaged)
+          {
+               g = 255;
+               b = 255;
+          }
+          glColor3ub(r, g, b);
+          glVertex2f(-0.25f, -0.1f);
+          glVertex2f(-0.4f, -0.4f);
+          glVertex2f(-0.1f, -0.4f);
+          glEnd();
+
+          break;
+     case 5: glBegin(GL_TRIANGLES); //Position 14
+          if (isdamaged)
+          {
+               g = 255;
+               b = 255;
+          }
+          glColor3ub(r, g, b);
+          glVertex2f(-0.25f, -0.6f);
+          glVertex2f(-0.4f, -0.9f);
+          glVertex2f(-0.1f, -0.9f);
+          glEnd();
+          break;
+
+     }
+}
+
+void turret(int num)
+{
+     GLubyte r = 102;
+     GLubyte g = 0;
+     GLubyte b = 204;
+
+     switch (num)
+     {
+     case 1:
+
+          //Position 2 Turret         
+          glBegin(GL_QUADS); 
+          glColor3ub(r, g, b); // set color purple
+          glVertex3f(-0.25f, .875f, 0.1f);
+          glVertex3f(-0.375, .75f, 0.1f);
+          glVertex3f(-0.25f, .625f, 0.1f);
+          glVertex3f(-0.125f, 0.75f, 0.1f);
+          glEnd();
+          break;
+
+     case 2:
+          //Position 15 Turret
+          glBegin(GL_QUADS);
+          glColor3ub(r, g, b); // set color purple
+          glVertex3f(0.25f, -.625f, 0.1f);
+          glVertex3f(0.125, -.75f, 0.1f);
+          glVertex3f(0.25f, -0.875f, 0.1f);
+          glVertex3f(0.375f, -0.75f, 0.1f);
+          glEnd();
+          break;
+     }
+}
+
+
+
+
 int main(void)
 {
      GLFWwindow* window;
@@ -312,38 +319,49 @@ int main(void)
 
      glfwMakeContextCurrent(window);
 
-     
-   
+
+
      if (glewInit() != GLEW_OK)
           std::cout << "Error!" << std::endl;
-
-     {
-          int argc = 1;
-          char* argv[1] = { (char*)"Something" };
-          glutInit(&argc, argv);
-     }
-
+    
      std::cout << glGetString(GL_VERSION) << std::endl;
 
      int x = 0;
+     int k = 0;
+     int click = 0;
+     int click2 = 0;
      double startTime = glfwGetTime();
      double xpos, ypos;
      /* Loop until the user closes the window */
+     
      while (!glfwWindowShouldClose(window))
      {
           double currentTime = glfwGetTime();
           int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
           glfwGetCursorPos(window, &xpos, &ypos);
-          if (state == GLFW_PRESS && xpos >= 250 && xpos <= 500 && ypos >= 0 && ypos <= 250)
-          {
-               turret(1);  //Doesn't seem to currently be working
-               Sleep(10000); //Only here to test if a mouse click pauses the animation
-               std::cout << "Tower 1 Placed!" << std::endl;
-          }
-          /* Render here */
 
           glClear(GL_COLOR_BUFFER_BIT);
+          
+          /* Render here */
+
+          
           map();
+          placeholders();
+          if (state == GLFW_PRESS && xpos >= 250 && xpos <= 500 && ypos >= 0 && ypos <= 250) //if tower 1 placeholder is selected, change "click to 1"
+          {
+               click = 1;               
+               std::cout << "Tower 1 Placed!" << std::endl;
+          }// tower 1 if statement
+          
+
+          if (state == GLFW_PRESS && xpos >= 500 && xpos <= 750 && ypos >= 750 && ypos <= 1000)
+          {
+               click2 = 2;
+               std::cout << "Tower 2 Placed!" << std::endl;
+          }
+
+          turret(click);
+          turret(click2);
           enemy(x);
           if (x < 5 && (currentTime - startTime) > x * 1)
                x += 1;
